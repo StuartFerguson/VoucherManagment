fc3986d89e7c9fb3b35503a3a10e9319eba0a075
Stuart Ferguson <stuart_ferguson1@btinternet.com>
2020-12-02 23:05:34 +0000

    Skeleton service

---
+++ VoucherManagement/Program.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VoucherManagement
+{
+    using System.Diagnostics.CodeAnalysis;
+    using System.IO;
+
+    [ExcludeFromCodeCoverage]
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Program.CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            Console.Title = "Voucher Management";
+
+            //At this stage, we only need our hosting file for ip and ports
+            IConfigurationRoot config = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+                                                                  .AddJsonFile("hosting.json", optional: true)
+                                                                  .AddJsonFile("hosting.development.json", optional: true)
+                                                                  .AddEnvironmentVariables().Build();
+
+            IHostBuilder hostBuilder = Host.CreateDefaultBuilder(args);
+            hostBuilder.ConfigureWebHostDefaults(webBuilder =>
+                                                 {
+                                                     webBuilder.UseStartup<Startup>();
+                                                     webBuilder.UseConfiguration(config);
+                                                     webBuilder.UseKestrel();
+                                                 });
+            return hostBuilder;
+        }
+
+    }
+}
---
+++ VoucherManagement/Startup.cs
@@ -0,0 +1,219 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VoucherManagement
+{
+    using System.IO;
+    using System.Net.Http;
+    using System.Reflection;
+    using System.Xml;
+    using Common;
+    using EventStore.Client;
+    using HealthChecks.UI.Client;
+    using Microsoft.AspNetCore.Authentication.JwtBearer;
+    using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+    using Microsoft.AspNetCore.Mvc.ApiExplorer;
+    using Microsoft.AspNetCore.Mvc.Versioning;
+    using Microsoft.Extensions.Diagnostics.HealthChecks;
+    using Microsoft.Extensions.Options;
+    using Microsoft.IdentityModel.Logging;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Serialization;
+    using Shared.General;
+    using Swashbuckle.AspNetCore.Filters;
+    using Swashbuckle.AspNetCore.SwaggerGen;
+
+    public class Startup
+    {
+        public Startup(IWebHostEnvironment webHostEnvironment)
+        {
+            IConfigurationBuilder builder = new ConfigurationBuilder().SetBasePath(webHostEnvironment.ContentRootPath)
+                                                                      .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                                                                      .AddJsonFile($"appsettings.{webHostEnvironment.EnvironmentName}.json", optional: true).AddEnvironmentVariables();
+
+            Startup.Configuration = builder.Build();
+            Startup.WebHostEnvironment = webHostEnvironment;
+        }
+
+        /// <summary>
+        /// Gets or sets the configuration.
+        /// </summary>
+        /// <value>
+        /// The configuration.
+        /// </value>
+        public static IConfigurationRoot Configuration { get; set; }
+
+        /// <summary>
+        /// Gets or sets the web host environment.
+        /// </summary>
+        /// <value>
+        /// The web host environment.
+        /// </value>
+        public static IWebHostEnvironment WebHostEnvironment { get; set; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        /// <summary>
+        /// Configures the services.
+        /// </summary>
+        /// <param name="services">The services.</param>
+        public void ConfigureServices(IServiceCollection services)
+        {
+            ConfigurationReader.Initialise(Startup.Configuration);
+
+            this.ConfigureMiddlewareServices(services);
+
+            ConfigurationReader.Initialise(Startup.Configuration);
+        }
+
+        private void ConfigureMiddlewareServices(IServiceCollection services)
+        {
+            services.AddHealthChecks()
+                    //.AddEventStore(Startup.EventStoreClientSettings,
+                    //               userCredentials: Startup.EventStoreClientSettings.DefaultCredentials,
+                    //               name: "Eventstore",
+                    //               failureStatus: HealthStatus.Unhealthy,
+                    //               tags: new string[] { "db", "eventstore" })
+                    .AddUrlGroup(new Uri($"{ConfigurationReader.GetValue("SecurityConfiguration", "Authority")}/health"),
+                                 name:"Security Service",
+                                 httpMethod:HttpMethod.Get,
+                                 failureStatus:HealthStatus.Unhealthy,
+                                 tags:new string[] {"security", "authorisation"});
+
+            services.AddApiVersioning(
+                                      options =>
+                                      {
+                                          // reporting api versions will return the headers "api-supported-versions" and "api-deprecated-versions"
+                                          options.ReportApiVersions = true;
+                                          options.DefaultApiVersion = new ApiVersion(1, 0);
+                                          options.AssumeDefaultVersionWhenUnspecified = true;
+                                          options.ApiVersionReader = new HeaderApiVersionReader("api-version");
+                                      });
+
+            services.AddVersionedApiExplorer(
+                                             options =>
+                                             {
+                                                 // add the versioned api explorer, which also adds IApiVersionDescriptionProvider service
+                                                 // note: the specified format code will format the version as "'v'major[.minor][-status]"
+                                                 options.GroupNameFormat = "'v'VVV";
+
+                                                 // note: this option is only necessary when versioning by url segment. the SubstitutionFormat
+                                                 // can also be used to control the format of the API version in route templates
+                                                 options.SubstituteApiVersionInUrl = true;
+                                             });
+
+            services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
+
+            services.AddSwaggerGen(c =>
+            {
+                // add a custom operation filter which sets default values
+                c.OperationFilter<SwaggerDefaultValues>();
+                c.ExampleFilters();
+            });
+
+            services.AddSwaggerExamplesFromAssemblyOf<SwaggerJsonConverter>();
+
+            IdentityModelEventSource.ShowPII = true;
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+                   .AddJwtBearer(options =>
+                   {
+                       //options.SaveToken = true;
+                       options.Authority = ConfigurationReader.GetValue("SecurityConfiguration", "Authority");
+                       options.Audience = ConfigurationReader.GetValue("SecurityConfiguration", "ApiName");
+                       options.RequireHttpsMetadata = false;
+                       options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
+                       {
+                           ValidateIssuer = true,
+                           ValidateAudience = true,
+                           ValidAudience = ConfigurationReader.GetValue("SecurityConfiguration", "ApiName"),
+                           ValidIssuer = ConfigurationReader.GetValue("SecurityConfiguration", "Authority"),
+                       };
+                       options.IncludeErrorDetails = true;
+                   });
+
+            services.AddControllers().AddNewtonsoftJson(options =>
+            {
+                options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+                options.SerializerSettings.TypeNameHandling = TypeNameHandling.Auto;
+                options.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
+                options.SerializerSettings.DateTimeZoneHandling = DateTimeZoneHandling.Utc;
+                options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            });
+
+            Assembly assembly = this.GetType().GetTypeInfo().Assembly;
+            services.AddMvcCore().AddApplicationPart(assembly).AddControllersAsServices();
+        }
+        
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// <summary>
+        /// Configures the specified application.
+        /// </summary>
+        /// <param name="app">The application.</param>
+        /// <param name="env">The env.</param>
+        /// <param name="loggerFactory">The logger factory.</param>
+        /// <param name="provider">The provider.</param>
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory,
+                              IApiVersionDescriptionProvider provider)
+        {
+            String nlogConfigFilename = "nlog.config";
+
+            if (env.IsDevelopment())
+            {
+                nlogConfigFilename = $"nlog.{env.EnvironmentName}.config";
+                app.UseDeveloperExceptionPage();
+            }
+
+            //loggerFactory.ConfigureNLog(Path.Combine(env.ContentRootPath, nlogConfigFilename));
+            //loggerFactory.AddNLog();
+
+            ILogger logger = loggerFactory.CreateLogger("VoucherManagement");
+
+            //Logger.Initialise(logger);
+
+            //app.AddRequestLogging();
+            //app.AddResponseLogging();
+            //app.AddExceptionHandler();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapHealthChecks("health", new HealthCheckOptions()
+                {
+                    Predicate = _ => true,
+                    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+                });
+            });
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(
+                             options =>
+                             {
+                                 // build a swagger endpoint for each discovered API version
+                                 foreach (ApiVersionDescription description in provider.ApiVersionDescriptions)
+                                 {
+                                     options.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json", description.GroupName.ToUpperInvariant());
+                                 }
+                             });
+        }
+    }
+}
