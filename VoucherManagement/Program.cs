c7440a8a5f946ff02b0d39fc18053a585b2a8d26
Stuart Ferguson <stuart_ferguson1@btinternet.com>
2021-03-17 09:21:55 +0000

    Subscription worker updated

--- VoucherManagement/Program.cs
+++ VoucherManagement/Program.cs
@@ -11,9 +11,13 @@ namespace VoucherManagement
 {
     using System.Diagnostics.CodeAnalysis;
     using System.IO;
+    using System.Net.Http;
+    using EventStore.Client;
     using Microsoft.Extensions.DependencyInjection;
     using Shared.EventStore.Aggregate;
+    using Shared.EventStore.EventHandling;
     using Shared.EventStore.Subscriptions;
+    using Shared.Logger;
     using Voucher.DomainEvents;
 
     [ExcludeFromCodeCoverage]
@@ -47,10 +51,48 @@ namespace VoucherManagement
 
                                               TypeProvider.LoadDomainEventsTypeDynamically();
 
-                                              services.AddHostedService<SubscriptionWorker>();
+                                              services.AddHostedService<SubscriptionWorker>(provider =>
+                                                                                            {
+                                                                                                IDomainEventHandlerResolver r =
+                                                                                                    provider.GetRequiredService<IDomainEventHandlerResolver>();
+                                                                                                EventStorePersistentSubscriptionsClient p = provider.GetRequiredService<EventStorePersistentSubscriptionsClient>();
+                                                                                                HttpClient h = provider.GetRequiredService<HttpClient>();
+                                                                                                SubscriptionWorker worker = new SubscriptionWorker(r, p, h);
+                                                                                                worker.TraceGenerated += Worker_TraceGenerated;
+                                                                                                return worker;
+                                                                                            });
                                           });
             return hostBuilder;
         }
 
+        /// <summary>
+        /// Workers the trace generated.
+        /// </summary>
+        /// <param name="trace">The trace.</param>
+        /// <param name="logLevel">The log level.</param>
+        private static void Worker_TraceGenerated(string trace, LogLevel logLevel)
+        {
+            switch (logLevel)
+            {
+                case LogLevel.Trace:
+                    Logger.LogTrace(trace);
+                    break;
+                case LogLevel.Debug:
+                    Logger.LogDebug(trace);
+                    break;
+                case LogLevel.Information:
+                    Logger.LogInformation(trace);
+                    break;
+                case LogLevel.Warning:
+                    Logger.LogWarning(trace);
+                    break;
+                case LogLevel.Error:
+                    Logger.LogError(new Exception(trace));
+                    break;
+                case LogLevel.Critical:
+                    Logger.LogCritical(new Exception(trace));
+                    break;
+            }
+        }
     }
 }
