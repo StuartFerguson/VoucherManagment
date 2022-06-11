8dacf6918c7bfa8a73d8017332a23510e64535ae
Stuart Ferguson <stuart_ferguson1@btinternet.com>
2022-06-11 22:28:51 +0100

    ..

--- VoucherManagement.IntegrationTests/Common/DockerHelper.cs
+++ VoucherManagement.IntegrationTests/Common/DockerHelper.cs
@@ -104,7 +104,7 @@ namespace VoucherManagement.IntegrationTests.Common
         {
             var name = estateName.Replace(" ", "");
             List<(string streamName, string groupName, Int32 numberOfRetries)> subscriptions = new ();
-            subscriptions.Add((name, "Reporting",0));
+            subscriptions.Add((name, "Reporting",5));
             subscriptions.Add(($"EstateManagementSubscriptionStream_{name}", "Estate Management",0));
             await this.PopulateSubscriptionServiceConfiguration(this.EventStoreHttpPort, subscriptions);
         }
